79e3c7ef9646e71983e7a25996deebbbda041806
MyronFields1 <68288662+MyronFields@users.noreply.github.com>
2020-07-20 17:17:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+num1 = range(10) # generates a random int between 0 and 10
+num2 = random.randint(1,6) # generates a random int between 1 and 6
+
+num1 = range(100) # generates a random int between 0 and 100
+num2 = random.randint(1,6) # generates a random int between 1 and 6
+
+num1 = range(100,000) # generates a random int between 0 and 100,000
+num2 = random.randint(1,6) # generates a random int between 1 and 6
+
+
+
+'{:10.00%}'.format(rate) # returns a string
